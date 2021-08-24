3f4e70383a1b0fedfb87f46e207b2b76479a65a3
Omar Kamel <30291302+TekExplorer@users.noreply.github.com>
2021-08-23 20:23:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -146,8 +146,8 @@ discordClient.on("message", (message) => {
 });
 
 expressApp.post(config.callbackURL, (req, res) => {
-	console.log("Req: " + req);
-	console.log("Res: " + res);
+	console.log("Req: " + JSON.stringify(req));
+	console.log("Res: " + JSON.stringify(res));
     if(req.body.name == config.groupme.name) return;
 
     var text = req.body.text;
